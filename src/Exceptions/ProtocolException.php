e357fa4c2f9e1a3e1b47874fc6bc647bcad51fba
Kyle Rinfreschi <krinfreschi@msn.com>
2014-07-06 22:35:22 +0200

    Fixed issue with throwing ProtocolExceptions.

--- src/Exceptions/ProtocolException.php
+++ src/Exceptions/ProtocolException.php
@@ -19,25 +19,9 @@ class ProtocolException extends \Exception {
 
     function __construct(Resource $resource, $message = "")
     {
-        if(!($protocol = $this->getCallingClass()) instanceof Protocol){
+        if(!($protocol = get_calling_class()) instanceof Protocol){
             throw new \RuntimeException("");
         }
-        $this->message = MessageEnvelope::make($resource, $protocol, $message);
-    }
-
-    protected function getCallingClass(){
-        //get the trace
-        $trace = debug_backtrace(DEBUG_BACKTRACE_PROVIDE_OBJECT | DEBUG_BACKTRACE_IGNORE_ARGS);
-
-        // Get the class that is asking for who awoke it
-        $class = $trace[1]['class'];
-
-        // +1 to i cos we have to account for calling this function
-        for ( $i=1; $i< count($trace); $i++ ) {
-            if ( isset( $trace[$i] ) ) // is it set?
-                if ( $class != $trace[$i]['class'] ){
-                    return $trace[$i]['object'];
-                } // is it a different class
-        }
+        $this->message = MessageEnvelope::make($resource, $message, $protocol);
     }
 }
